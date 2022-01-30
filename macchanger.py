acf98d105e302f766092a5bd1ac23893c877ea7c
Cunoo <40718112+Cunoo@users.noreply.github.com>
2022-01-31 00:20:51 +0100

    Update macchanger.py

--- macchanger.py
+++ macchanger.py
@@ -1,13 +1,48 @@
+#!/bin/python3
+
 import subprocess
+import optparse
+import re
 
-#first you must know name of Wireless adapter, just type ifconfig ...
+def get_arguments():
+	parser = optparse.OptionParser()
+	parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC ADRESS")
+	parser.add_option("-m", "--mac", dest="new_mac", help="New MAC ADRESS")
+	(options, arguments) = parser.parse_args()
+	if not options.interface:
+		parser.error("[-] Please specifiy an interface, use --help for more info")
+		
+	elif not options.new_mac:
+		parser.error("[-] Please specifiy an new mac address, use --help for more info")
+	return options
+		
 
+def change_mac(interface,new_mac):
+	print("-------Changing MAC ADDRES for " + interface + " to " + new_mac, "-------\n")
 
-print("Create by Cunoo\n if yo \nShutdown Wireless adapter... \n")
-subprocess.call(["sudo", "ifconfig", "enter name of wireless adapter", "down"])
+	subprocess.call(["ifconfig" ,interface,"down"])
+	subprocess.call(["ifconfig" ,interface, "hw", "ether" ,new_mac])
+	subprocess.call(["ifconfig" ,interface, "up"])
 
-macadress = input("please write macadress thanks: \n")
-subprocess.call(["sudo", "ifconfig", "enter name of wireless adapter", "hw", "ether", macadress])
+def get_current_mac(interface):
+	ifconfig_result = subprocess.check_output(["ifconfig", interface])
 
-subprocess.call(["sudo", "ifconfig", "enter name of wireless adapter", "up"])
+	mac_addres_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result.decode("utf-8"))
+	if mac_addres_result:
+		return mac_addres_result.group(0)
+	else:
+		print("\n\nCould not read MAC addres!!!!!!")
 
+options = get_arguments()
+
+current_mac = get_current_mac(options.interface)
+print("Current MAC : " + str(current_mac))
+	
+change_mac(options.interface, options.new_mac)
+
+##Rewrite prevous variable
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+	print("[+] MAC ADDRESS was succesfully changed to " + current_mac)
+else:
+	print("[-] MAC address did not get changed")
